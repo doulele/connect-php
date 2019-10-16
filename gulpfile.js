49d26a05710b427e208765da4aed32060192254b
野生小猿猿 <1220736335@qq.com>
2019-10-16 11:54:00 +0800

    php服务器+浏览器自动刷新

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp=require('gulp'),
+	connectPhp=require('gulp-connect-php'),
+	htmlmin=require('gulp-htmlmin'),
+	browserSync=require('browser-sync').create();
+
+gulp.task('html',function(){
+	return gulp.src('develop/*html')
+		.pipe(htmlmin({collapseWhitespace: true}))
+		.pipe(gulp.dest('product/'))
+		.pipe(browserSync.reload({stream:true}))
+})
+gulp.task('server',function(){
+	var options={
+		base:'./product',
+		bin:'E:/wamp/bin/php/php5.5.12/php.exe',//自己php.exe的地址
+		ini:'E:/wamp/bin/php/php5.5.12/php.ini',
+		port:6666
+	}
+	return connectPhp.server(options)
+})
+gulp.task('browser-sync',function(){
+	var options={
+		proxy:'127.0.0.1:6666',
+		port:3000,
+		watch:true,
+		startPath:'test.html',
+		open:true
+	};
+	return browserSync.init(options);
+})
+gulp.task('watch',function(){
+	gulp.watch('develop/*html',gulp.series('html'))
+})
+gulp.task('default',gulp.series('html',gulp.parallel('server','browser-sync','watch')));
